07d8eb710f7a8546245026ed67f67ebde4acbcac
MohanSiddhardha <mohangude5@gmail.com>
2023-04-23 19:10:10 +0530

    Refactor ChatClient.cs for improved readability
    This commit removes unnecessary lines and reorganizes the code in the ChatClient class to make it easier to read and understand. The changes do not affect the functionality of the code.

--- ChatClient.cs
+++ ChatClient.cs
@@ -1,58 +1,3 @@
-// using System;
-// using System.Net;
-// using System.Net.Sockets;
-// using System.Text;
-// using System.Threading;
-
-// class ClientSocket
-// {
-//     static Socket Client;
-//     static bool IsRunning = true;
-//     static void Main(string [] args)
-//     {
-//         string ServerAddress = "127.0.0.1";
-//         int ServerPort = 5987;
-
-//         Client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-//         IPEndPoint EndPoint = new IPEndPoint(IPAddress.Parse(ServerAddress), ServerPort);
-//         Client.Connect(EndPoint);
-
-//         Thread SendThread = new Thread(SendMessage);
-//         Thread ReceiveThread = new Thread(ReceiveMessage);
-
-//         SendThread.Start();
-//         ReceiveThread.Start();
-
-//         SendThread.Join();
-//         ReceiveThread.Join();
-
-//         Client.Shutdown(SocketShutdown.Both);
-//         Client.Close();
-//     }
-
-//     static void SendMessage()
-//     {
-//         while (IsRunning)
-//         {
-//             string Message = Console.ReadLine();
-//             byte[] MessageBytes = Encoding.ASCII.GetBytes(Message);
-//             Client.Send(MessageBytes);
-//         }
-//     }
-
-//     static void ReceiveMessage()
-//     {
-//         while (IsRunning)
-//         {
-//             byte[] Buffer = new byte[1024];
-//             int BytesReceived = Client.Receive(Buffer);
-//             string ReceivedMessage = Encoding.ASCII.GetString(Buffer, 0, BytesReceived);
-//             Console.WriteLine(ReceivedMessage);
-//         }
-//     }
-// }
-
-
 using System;
 using System.Net;
 using System.Net.Sockets;
@@ -61,26 +6,48 @@ using System.Threading;
 
 class ClientSocket
 {
-    bool Running = true;
-    static void Main(string[] args)
+    static Socket Client;
+    static bool IsRunning = true;
+    static void Main(string [] args)
     {
-        Socket Client
-        string ServerIpAddress = "localhost";
+        string ServerAddress = "127.0.0.1";
         int ServerPort = 5987;
-        Client = new Socket(AddressFamily, InterNetwork, SocketTypeStream, ProtocolType.Tcp);
-        IpEndPoint Endpoint = new IpEndPoint(IpAddress.Parse(ServerIpAddress), ServerPort);
-        Client.connect(Endpoint);
 
-        Thread SendThread = new Thread()
+        Client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        IPEndPoint EndPoint = new IPEndPoint(IPAddress.Parse(ServerAddress), ServerPort);
+        Client.Connect(EndPoint);
+
+        Thread SendThread = new Thread(SendMessage);
+        Thread ReceiveThread = new Thread(ReceiveMessage);
+
+        SendThread.Start();
+        ReceiveThread.Start();
+
+        SendThread.Join();
+        ReceiveThread.Join();
+
+        Client.Shutdown(SocketShutdown.Both);
+        Client.Close();
     }
 
     static void SendMessage()
     {
-        while (Running)
+        while (IsRunning)
         {
             string Message = Console.ReadLine();
             byte[] MessageBytes = Encoding.ASCII.GetBytes(Message);
-            Client.Send(Message);
+            Client.Send(MessageBytes);
+        }
+    }
+
+    static void ReceiveMessage()
+    {
+        while (IsRunning)
+        {
+            byte[] Buffer = new byte[1024];
+            int BytesReceived = Client.Receive(Buffer);
+            string ReceivedMessage = Encoding.ASCII.GetString(Buffer, 0, BytesReceived);
+            Console.WriteLine(ReceivedMessage);
         }
     }
 }
